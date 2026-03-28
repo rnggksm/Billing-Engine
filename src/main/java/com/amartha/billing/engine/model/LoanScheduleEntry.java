c9f2067bddcb4e1489ca30faf1bb9dd5cd398624
Rangga Kusuma <ranggavskusuma@gmail.com>
2026-03-28 21:58:39 +0700

    add fields on LoanScheduleEntry

--- src/main/java/com/amartha/billing/engine/model/LoanScheduleEntry.java
+++ src/main/java/com/amartha/billing/engine/model/LoanScheduleEntry.java
@@ -1,4 +1,29 @@
 package com.amartha.billing.engine.model;
 
 public class LoanScheduleEntry {
+  private final int weekNumber;
+  private final double amount;
+  private boolean paid;
+
+  public LoanScheduleEntry(int weekNumber, double amount) {
+    this.weekNumber = weekNumber;
+    this.amount = amount;
+    this.paid = false;
+  }
+
+  public int getWeekNumber() {
+    return weekNumber;
+  }
+
+  public double getAmount() {
+    return amount;
+  }
+
+  public boolean isPaid() {
+    return paid;
+  }
+
+  public void setPaid(boolean paid) {
+    this.paid = paid;
+  }
 }
