c519d70d9fd3d0407e2e5b0f658726e9c992bdb4
Rangga Kusuma <ranggavskusuma@gmail.com>
2026-03-28 21:56:16 +0700

    add fields on Loan

--- src/main/java/com/amartha/billing/engine/model/Loan.java
+++ src/main/java/com/amartha/billing/engine/model/Loan.java
@@ -1,4 +1,35 @@
 package com.amartha.billing.engine.model;
 
 public class Loan {
+  private final int loanId;
+  private final double principal;
+  private final double interestRate;
+  private int currentWeek;
+
+  public Loan(int loanId, double principal, double interestRate) {
+    this.loanId = loanId;
+    this.principal = principal;
+    this.interestRate = interestRate;
+    this.currentWeek = 0;
+  }
+
+  public int getLoanId() {
+    return loanId;
+  }
+
+  public double getPrincipal() {
+    return principal;
+  }
+
+  public double getInterestRate() {
+    return interestRate;
+  }
+
+  public int getCurrentWeek() {
+    return currentWeek;
+  }
+
+  public void setCurrentWeek(int currentWeek) {
+    this.currentWeek = currentWeek;
+  }
 }
